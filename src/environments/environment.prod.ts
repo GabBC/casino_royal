ae0804c073c76b8b07f1820933ba3c23a4517654
Gabos <gabriel.benniks@gmail.com>
2025-05-20 12:29:04 +0200

    adding environment and database

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://TON_URL.supabase.co',
+  supabaseKey: 'TA_CLE_ANNON'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://klhqxkshzvcbvswbxqhp.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtsaHF4a3NoenZjYnZzd2J4cWhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5NTIxMzUsImV4cCI6MjA2MTUyODEzNX0.PA0vDDPzcW9zjfer8_lsypTNM971qFUvx1rW-eNhkfw'
+};
