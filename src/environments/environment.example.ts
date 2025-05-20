fa6a1e867e646eb2133e2352f09b7130ae99f797
Gabos <gabriel.benniks@gmail.com>
2025-05-20 17:51:40 +0200

    adding an example of environment.ts and environment.prod.ts

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,7 @@
+/* this file is an example of how to use environment variable in this project */
+
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://project.supabase.co',
+  supabaseKey: 'PUBLIC_ANON_KEY',
+};
